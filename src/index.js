3136c690126e496bcf31dd7d1f229a51567ca473
Dmitry Gurinovich <dmitry.gurinovich@hotmail.com>
2023-01-03 20:53:10 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n < 0) {
+        return Number(String(n * (-1)).split('').reverse().join(''));
+    } else {
+        return Number(String(n).split('').reverse().join(''));
+    }
 }
